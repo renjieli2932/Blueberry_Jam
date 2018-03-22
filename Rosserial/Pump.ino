95541b79290aeb76d5d4d3f43aba763d780ff291
Renjie "Jimmy" Li <31699874+renjieli2932@users.noreply.github.com>
2018-03-21 23:50:01 -0400

    Not tested yet

---
+++ Rosserial/Pump.ino
@@ -0,0 +1,57 @@
+/*
+ * rosserial Subscriber
+ * On/Off Control 
+ * Jamming Gripper Pump
+ */
+
+#include <ros.h>
+#include <std_msgs/Empty.h>
+
+ros::NodeHandle nh;
+
+// connect motor controller pins to Arduino digital pins
+// Here I assume only one motor is needed
+// Remove the jumper on L298N first to PWN 
+int enA = 10; 
+int in1 = 9;
+int in2 = 8;
+
+void pump_on( const std_msgs::Empty& ros_msg){
+  digitalWrite(13, HIGH); //LED On   
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2,LOW);
+  analogWrite(enA,200) // Speed Range 0-255
+  //delay(10000); // delay 10s (test)
+}
+
+
+void pump_off( const std_msgs::Empty& ros_msg){
+  digitalWrite(13, LOW);  //LED Off
+  digitalWrite(in1,LOW);  //Turn off the motor
+  digitalWrite(in2,LOW);
+}
+
+ros::Subscriber<std_msgs::Empty> sub("pump_on", &pump_on );
+ros::Subscriber<std_msgs::Empty> sub("pump_off", &pump_off );
+
+void setup()
+{
+  pinMode(enA,OUTPUT); // PWM 
+  pinMode(in1,OUTPUT);
+  pinMode(in2,OUTPUT);
+  pinMode(13, OUTPUT); // LED, status monitor
+  // Initialization
+  digitalWrite(enA,LOW);
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,LOW);
+  digitalWrite(13,LOW);
+
+  nh.initNode();
+  nh.subscribe(sub);
+}
+
+void loop()
+{
+  nh.spinOnce();
+  delay(1);
+}
