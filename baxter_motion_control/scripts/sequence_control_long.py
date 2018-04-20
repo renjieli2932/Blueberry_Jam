cb1fa325d8915f6e7073d986cc2b904daf0a7033
Renjie Li <jimmyleeyog@gmail.com>
2018-04-20 16:28:23 -0400

    Add files via upload

--- baxter_motion_control/scripts/sequence_control_long.py
+++ baxter_motion_control/scripts/sequence_control_long.py
@@ -45,41 +45,42 @@ import baxter_interface
 
 class PickAndPlace():
     def __init__(self, shape, desired_position, limb, hover_distance = 0.15, verbose=False):
-        self._limb_name = limb # string
-        self._shape_name = shape # string
-        self._desired_position = desired_position # list[x,y] (integer)
-        self._hover_distance = hover_distance # in meters
-        self._verbose = verbose # bool
-        self._limb = baxter_interface.limb.Limb(limb)
-        self._limb_joint_names = self._limb.joint_names()
-        self.prev_time = 0.0
-        self.control_effort_x = 0.0
-        self.control_effort_y = 0.0
-        self.object_position = Object()
-        self.hover_position = Pose()
-	self.diff_old = 0.0
-	self.zposi = 0.0
+		self._limb_name = limb # string
+		self._shape_name = shape # string
+		self._desired_position = desired_position # list[x,y] (integer)
+		self._hover_distance = hover_distance # in meters
+		self._verbose = verbose # bool
+		self._limb = baxter_interface.limb.Limb(limb)
+		self._limb_joint_names = self._limb.joint_names()
+		self.prev_time = 0.0
+		self.control_effort_x = 0.0
+		self.control_effort_y = 0.0
+		self.object_position = Object()
+		self.hover_position = Pose()
+		self.diff_old = 0.0
+		self.zposi = 0.0
+		self.flag = 1
 
-        self.step = 0
-        self._rate = 10 #10Hz
-	self.display_pub= rospy.Publisher('/robot/xdisplay',Image,queue_size=60)
-	self.pub_grasp_now = rospy.Publisher("pump_on",Empty,queue_size=1)
-	self.pub_release_now = rospy.Publisher("pump_off",Empty,queue_size=1)
-	self.sub = rospy.Subscriber('/detected_image', Image,self.republish,None,1)
-	#msg_grasp_now = Bool(False)
-        #self.pub_grasp_now.publish(msg_grasp_now)
+		self.step = 0
+		self._rate = 10 #10Hz
+		self.display_pub= rospy.Publisher('/robot/xdisplay',Image,queue_size=60)
+		self.pub_grasp_now = rospy.Publisher("pump_on",Empty,queue_size=1)
+		self.pub_release_now = rospy.Publisher("pump_off",Empty,queue_size=1)
+		self.sub = rospy.Subscriber('/detected_image', Image,self.republish,None,1)
+		#msg_grasp_now = Bool(False)
+		#self.pub_grasp_now.publish(msg_grasp_now)
 
-        ns = "ExternalTools/" + limb + "/PositionKinematicsNode/IKService"
-        self._iksvc = rospy.ServiceProxy(ns, SolvePositionIK)
-        rospy.wait_for_service(ns, 5.0)
+		ns = "ExternalTools/" + limb + "/PositionKinematicsNode/IKService"
+		self._iksvc = rospy.ServiceProxy(ns, SolvePositionIK)
+		rospy.wait_for_service(ns, 5.0)
         # verify robot is enabled
-        print("Getting robot state... ")
-        self._rs = baxter_interface.RobotEnable(baxter_interface.CHECK_VERSION)
-        self._init_state = self._rs.state().enabled
-        print("Enabling robot... ")
-        self._rs.enable()
-        print("Moving to neutral pose...")
-        self._limb.move_to_neutral()
+		print("Getting robot state... ")
+		self._rs = baxter_interface.RobotEnable(baxter_interface.CHECK_VERSION)
+		self._init_state = self._rs.state().enabled
+		print("Enabling robot... ")
+		self._rs.enable()
+		print("Moving to neutral pose...")
+		self._limb.move_to_neutral()
 
 
     def republish(self,msg):
@@ -145,15 +146,15 @@ class PickAndPlace():
         rospy.sleep(1.0)
 
     def move_to_hover_2(self):
-        print("Moving the {0} arm to start pose...".format(self._limb_name))
-        hover = copy.deepcopy(self.object_pose3D)
-        # approach with a pose the hover-distance above the requested pose
-        hover.position.z = hover.position.z + self._hover_distance
-	hover.position.y = hover.position.y + 0.2
-        joint_angles = self.ik_request(hover)
-        self._guarded_move_to_joint_position(joint_angles)
-        self.step = self.step + 1
-        rospy.sleep(1.0)
+		print("Moving the {0} arm to start pose...".format(self._limb_name))
+		hover = copy.deepcopy(self.object_pose3D)
+		# approach with a pose the hover-distance above the requested pose
+		hover.position.z = hover.position.z + self._hover_distance
+		hover.position.y = hover.position.y + 0.2
+		joint_angles = self.ik_request(hover)
+		self._guarded_move_to_joint_position(joint_angles)
+		self.step = self.step + 1
+		rospy.sleep(1.0)
 
     def change_layer(self, delta_z):
         current_pose = self._limb.endpoint_pose()
@@ -170,18 +171,18 @@ class PickAndPlace():
         self.step = self.step + 1
 
     def approach(self, adjust_value= [0.0,0.0,0.0]):
-        current_pose = self._limb.endpoint_pose()
-        ik_pose = Pose()
-        ik_pose.position.x = current_pose['position'].x - 0.055 + adjust_value[0]
-        ik_pose.position.y = current_pose['position'].y + 0.015 + adjust_value[1]
-        ik_pose.position.z = current_pose['position'].z
-        ik_pose.orientation.x = current_pose['orientation'].x
-        ik_pose.orientation.y = current_pose['orientation'].y
-        ik_pose.orientation.z = current_pose['orientation'].z
-        ik_pose.orientation.w = current_pose['orientation'].w
-        joint_angles = self.ik_request(ik_pose)
-        self._guarded_move_to_joint_position(joint_angles)
-	'''
+		current_pose = self._limb.endpoint_pose()
+		ik_pose = Pose()
+		ik_pose.position.x = current_pose['position'].x - 0.055 + adjust_value[0]
+		ik_pose.position.y = current_pose['position'].y + 0.015 + adjust_value[1]
+		ik_pose.position.z = current_pose['position'].z
+		ik_pose.orientation.x = current_pose['orientation'].x
+		ik_pose.orientation.y = current_pose['orientation'].y
+		ik_pose.orientation.z = current_pose['orientation'].z
+		ik_pose.orientation.w = current_pose['orientation'].w
+		joint_angles = self.ik_request(ik_pose)
+		self._guarded_move_to_joint_position(joint_angles)
+		'''
         current_pose = self._limb.endpoint_pose()
         ik_pose = Pose()
         ik_pose.position.x = current_pose['position'].x
@@ -193,30 +194,30 @@ class PickAndPlace():
         ik_pose.orientation.w = current_pose['orientation'].w
         joint_angles = self.ik_request(ik_pose)
         self._guarded_move_to_joint_position(joint_angles)
-	'''
+		'''
 	# adjust angle (counter moment)
-        current_pose = self._limb.endpoint_pose()
-        quaternion = (current_pose['orientation'].x,
-	        current_pose['orientation'].y,
-	        current_pose['orientation'].z,
-	        current_pose['orientation'].w)
-	euler = tf.transformations.euler_from_quaternion(quaternion)
-	theta = euler[1] - 0.0*math.pi/180.0
-	q = tf.transformations.quaternion_from_euler(euler[0], theta, euler[2], axes='sxyz')
-	current_pose = self._limb.endpoint_pose()
-        ik_pose = Pose()
-        ik_pose.position.x = current_pose['position'].x
-        ik_pose.position.y = current_pose['position'].y
-	ik_pose.position.z = self.object_pose3D.position.z + adjust_value[2]
-        #ik_pose.position.z = current_pose['position'].z
-        ik_pose.orientation.x = q[0]
-        ik_pose.orientation.y = q[1]
-        ik_pose.orientation.z = q[2]
-        ik_pose.orientation.w = q[3]
-        joint_angles = self.ik_request(ik_pose)
-        self._guarded_move_to_joint_position(joint_angles)
+		current_pose = self._limb.endpoint_pose()
+		quaternion = (current_pose['orientation'].x,
+					current_pose['orientation'].y,
+					current_pose['orientation'].z,
+	    			current_pose['orientation'].w)
+		euler = tf.transformations.euler_from_quaternion(quaternion)
+		theta = euler[1] - 0.0*math.pi/180.0
+		q = tf.transformations.quaternion_from_euler(euler[0], theta, euler[2], axes='sxyz')
+		current_pose = self._limb.endpoint_pose()
+		ik_pose = Pose()
+		ik_pose.position.x = current_pose['position'].x
+		ik_pose.position.y = current_pose['position'].y
+		ik_pose.position.z = self.object_pose3D.position.z + adjust_value[2]
+		#ik_pose.position.z = current_pose['position'].z
+		ik_pose.orientation.x = q[0]
+		ik_pose.orientation.y = q[1]
+		ik_pose.orientation.z = q[2]
+		ik_pose.orientation.w = q[3]
+		joint_angles = self.ik_request(ik_pose)
+		self._guarded_move_to_joint_position(joint_angles)
 
-        self.step = self.step + 1
+		self.step = self.step + 1
 
     def adjust(self,gain):
         now_time = time.time()
@@ -235,27 +236,27 @@ class PickAndPlace():
             print("ERROR: INVALID SHAPE")
 
         if center:
-	    
-	    current_pose = self._limb.endpoint_pose()
-	    if current_pose['position'].z < 0.03:
-	    	self.zposi = 1.0
-	    else:
-	    	self.zposi = 0.0
-	    print(current_pose['position'].z)
-            # Subscribe Image from camera
-            rospy.Subscriber("/pixel_x/control_effort", Float64, pnp.callback_control_effort_x)
-            rospy.Subscriber("/pixel_y/control_effort", Float64, pnp.callback_control_effort_y)
-            print(self.control_effort_x*gain)
-            print(self.control_effort_y*gain)
+			current_pose = self._limb.endpoint_pose()
+			if current_pose['position'].z < 0.03:
+				self.zposi = 0.0
+			else:
+				self.zposi = 0.0
+			print(current_pose['position'].z)
+		    
+			# Subscribe Image from camera
+			rospy.Subscriber("/pixel_x/control_effort", Float64, pnp.callback_control_effort_x)
+			rospy.Subscriber("/pixel_y/control_effort", Float64, pnp.callback_control_effort_y)
+			print(self.control_effort_x*gain)
+			print(self.control_effort_y*gain)
             
-            ik_pose = Pose()
-            ik_pose.position.x = current_pose['position'].x + self.control_effort_y*gain
-            ik_pose.position.y = current_pose['position'].y + self.control_effort_x*gain
-            ik_pose.position.z = current_pose['position'].z - self.zposi #self.object_pose3D.position.z
-            ik_pose.orientation.x = current_pose['orientation'].x
-            ik_pose.orientation.y = current_pose['orientation'].y
-            ik_pose.orientation.z = current_pose['orientation'].z
-            ik_pose.orientation.w = current_pose['orientation'].w
+			ik_pose = Pose()
+			ik_pose.position.x = current_pose['position'].x + self.control_effort_y*gain
+			ik_pose.position.y = current_pose['position'].y + self.control_effort_x*gain
+			ik_pose.position.z = current_pose['position'].z - self.zposi #self.object_pose3D.position.z
+			ik_pose.orientation.x = current_pose['orientation'].x
+			ik_pose.orientation.y = current_pose['orientation'].y
+			ik_pose.orientation.z = current_pose['orientation'].z
+			ik_pose.orientation.w = current_pose['orientation'].w
             joint_angles = self.ik_request(ik_pose)
 
             #self._guarded_move_to_joint_position(joint_angles)
@@ -267,12 +268,11 @@ class PickAndPlace():
             print('desired=[{0},{1}]'.format(desired_position[0],desired_position[1]))
             print('desired=[{0},{1}]'.format(diff[0],diff[1]))
             if np.linalg.norm(diff) <= 7:
-		#count = count + 1
-		#if
-                self.step = self.step + 1
-	    self.diff_old = diff
-            pub_state = rospy.Publisher("/pixel_x/state",Float64,queue_size=1)
-            msg_state = Float64(center[0])
+		        self.step = self.step + 1
+				self.diff_old = diff
+		    
+			pub_state = rospy.Publisher("/pixel_x/state",Float64,queue_size=1)
+			msg_state = Float64(center[0])
             pub_state.publish(msg_state)
             pub_state = rospy.Publisher("/pixel_y/state",Float64,queue_size=1)
             msg_state = Float64(center[1])
@@ -284,6 +284,33 @@ class PickAndPlace():
             pub_setpoint = rospy.Publisher("/pixel_y/setpoint",Float64,queue_size=1)
             msg_setpoint = Float64(desired_position[1])
             pub_setpoint.publish(msg_setpoint)
+	
+	else:
+	    print('search')	
+	    current_pose = self._limb.endpoint_pose()
+	    
+	    if self.flag == 1:
+            search_x = 0.5
+            search_y = 0.0
+	    	self.flag = -1
+	    elif self.flag == -1:
+            search_x = 0.65
+	        search_y = 0.0
+  			self.flag = 1
+        
+  	    print(current_pose['position'].x)
+	    
+	    ik_pose = Pose()
+        ik_pose.position.x = search_x
+        ik_pose.position.y = current_pose['position'].y
+        ik_pose.position.z = current_pose['position'].z 
+        ik_pose.orientation.x = current_pose['orientation'].x
+        ik_pose.orientation.y = current_pose['orientation'].y
+        ik_pose.orientation.z = current_pose['orientation'].z
+        ik_pose.orientation.w = current_pose['orientation'].w
+        joint_angles = self.ik_request(ik_pose)
+	    self._guarded_move_to_joint_position(joint_angles)
+	    #self._limb.set_joint_positions(joint_angles)
 
     def retract(self):
         # retrieve current pose from endpoint
@@ -307,7 +334,7 @@ class PickAndPlace():
     def grasp(self):
         #msg_grasp_now = Bool(True)
         #self.pub_grasp_now.publish(msg_grasp_now)
-	self.pub_grasp_now.publish()
+		self.pub_grasp_now.publish()
         rospy.sleep(10.0)
 	# TEST
 	#self.pub_release_now.publish()
@@ -317,77 +344,42 @@ class PickAndPlace():
     def release(self):
         #msg_grasp_now = Bool(True)
         #self.pub_grasp_now.publish(msg_grasp_now)
-	self.pub_release_now.publish()
-	rospy.sleep(3.0)
+		self.pub_release_now.publish()
+		rospy.sleep(3.0)
 	# TEST OVER
         self.step = self.step + 1
 	
     def main(self):
         rate = rospy.Rate(self._rate)
-
-        # Motion Start
+		# Motion Start
         while not rospy.is_shutdown():
             if self.step == 0:	    
 	    	print("\nHovering...")
 	        self.move_to_hover()
-	    
-	    #elif self.step == 1:
-	    #	 print("\nAdjusting...")
-            #    self.adjust(0.0003)
             elif self.step == 1:
                 print("\nChange Layer...")
-                self.change_layer(0.0)
-	    #elif self.step == 3:
-	    #	print("\nAdjusting...")
-            #    self.adjust(0.0003)
-            #elif self.step == 4:
-            #    print("\nChange Layer...")
-            #    self.change_layer(0.0)
-            #elif self.step == 5:
-	    #    print("\nAdjusting...")
-            #    self.adjust(0.0003)
-            #elif self.step == 6:
-            #    print("\nChange Layer...")
-            #    self.change_layer(0.0)
+                self.change_layer(0.07)
             elif self.step == 2:
-		print("\nAdjusting...")
-                self.adjust(0.0003)
-		                
-	    elif self.step == 3:
-                print("\nApproaching...")
-                self.approach()
-	    elif self.step == 4:
-                print("\nGrasping...")
+			print("\nAdjusting...")
+		        self.adjust(0.0003)
+			elif self.step == 3:
+		        print("\nApproaching...")
+		        self.approach()
+			elif self.step == 4:
+		        print("\nGrasping...")
                 self.grasp()
             elif self.step == 5:
                 print("\nRetracting...")
                 self.retract()
-	    elif self.step == 6:
-	    	print("\nMoving to the hole...")
-	        self.move_to_hover_2()
-
-            #elif self.step == 7:
-            #    print("\nAdjusting...")
-            #    self.adjust(0.0003)            
+	    	elif self.step == 6:
+	    		print("\nMoving to the hole...")
+	        	self.move_to_hover_2()
             elif self.step == 7:
                 print("\nChange Layer...")
-                self.change_layer(0.0)
-	    elif self.step == 8:
-		print("\nAdjusting...")
+                self.change_layer(0.06)
+	    	elif self.step == 8:
+			print("\nAdjusting...")
                 self.adjust(0.0003)
-            #elif self.step == 10:
-            #    print("\nChange Layer...")
-            #    self.change_layer(0.02)
-            #elif self.step == 11:
-	    #    print("\nAdjusting...")
-            #    self.adjust(0.0003)
-            #elif self.step == 12:
-            #    print("\nChange Layer...")
-            #    self.change_layer(0.02)
-            #elif self.step == 13:
-	    #    print("\nAdjusting...")
-            #    self.adjust(0.0003)
-	
             elif self.step == 9:
                 print("\nApproaching...")
                 self.approach([0.005,-0.005,0.05])
@@ -397,9 +389,8 @@ class PickAndPlace():
             elif self.step == 11:
                 print("\nRetracting...")
                 self.retract()
-
             else:
-                self.pause()
+            #    self.pause()
                 break
             rate.sleep()
 
@@ -460,34 +451,37 @@ if __name__ == '__main__':
     # rospy.wait_for_message("/robot/sim/started", Empty)
 
     limb = 'left'
-    shape = 'circle'
+    shapes = ['circle', 'square','triangle']
+    
     hover_distance = 0.15
     desired_position = [320,250]
-    pnp = PickAndPlace(shape,desired_position,limb,hover_distance)
+    for shape in shapes:
+		print(shape)
+		pnp = PickAndPlace(shape, desired_position,limb,hover_distance)
+		
+		pub_pid_enable = rospy.Publisher("/pid_enable",Bool,queue_size=1)
+		msg_pid_enable = Bool(True)
+		pub_pid_enable.publish(msg_pid_enable)
 
-    pub_pid_enable = rospy.Publisher("/pid_enable",Bool,queue_size=1)
-    msg_pid_enable = Bool(True)
-    pub_pid_enable.publish(msg_pid_enable)
+		# Subscribe Image from camera
+		rospy.Subscriber("/detected_object", Object, pnp.callback_camera)
 
-    # Subscribe Image from camera
-    rospy.Subscriber("/detected_object", Object, pnp.callback_camera)
+		# Subscribe Image from camera
+		# rospy.Subscriber("/control_effort", Float64, pnp.callback_control_effort)
 
-    # Subscribe Image from camera
-    # rospy.Subscriber("/control_effort", Float64, pnp.callback_control_effort)
+		# Subscribe Image from kinect
+		#rospy.Subscriber("/xxxxxxx", Pose, pnp.callback_kinect)
+		quaternion = tf.transformations.quaternion_from_euler(0.0, math.pi, 0.0, axes='sxyz')
+		hover = Pose()
+		hover.position.x = 0.6
+		hover.position.y = 0.1
+		hover.position.z = -0.04
+		hover.orientation.x = quaternion[0]
+		hover.orientation.y = quaternion[1]
+		hover.orientation.z = quaternion[2]
+		hover.orientation.w = quaternion[3]
+		pnp.callback_kinect(hover)
 
-    # Subscribe Image from kinect
-    #rospy.Subscriber("/xxxxxxx", Pose, pnp.callback_kinect)
-    quaternion = tf.transformations.quaternion_from_euler(0.0, math.pi, 0.0, axes='sxyz')
-    hover = Pose()
-    hover.position.x = 0.6
-    hover.position.y = 0.1
-    hover.position.z = -0.04
-    hover.orientation.x = quaternion[0]
-    hover.orientation.y = quaternion[1]
-    hover.orientation.z = quaternion[2]
-    hover.orientation.w = quaternion[3]
-    pnp.callback_kinect(hover)
-
-    pnp.main()
+		pnp.main()
 
     rospy.spin()
